d971fee664d41cc3b0719f2e1620bfd76204464b
Ro Anders <ro.c.anders@gmail.com>
2018-05-19 09:04:33 -0400

    Got most of tutorial coded.  Players fire and damage each other.

---
+++ Intro to Simple Multiplayer/Assets/Scenes/Billboard.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+    }
+}
