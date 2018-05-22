5c9c5319e9d4d0dc566b5b65adb77acbce216631
Robert Antonucci <Robert.Antonucci@washpost.com>
2018-05-22 05:49:07 -0400

    Created a chat box trial project

---
+++ chatbox/chatbox/Assets/Scripts/Chat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour {
+
+    public InputField input;
+    public Text output;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return) && (input.text != "")) {
+            output.text += input.text + "\n";
+            input.text = "";
+        }
+	}
+}
