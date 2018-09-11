b58f9d3e2b77b311cab2307aeb8978b210f5c2f5
Robert Antonucci <Robert.Antonucci@washpost.com>
2018-09-11 16:48:24 -0400

    Retrying controlling Unity with Git

--- Intro to Simple Multiplayer/Assets/Scenes/Billboard.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Billboard : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(Camera.main.transform);
-    }
-}
