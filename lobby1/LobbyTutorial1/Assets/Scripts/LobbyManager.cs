26163330ef556a0e32975fd0c5a0b7fcf2784025
Ro Anders <ro.c.anders@gmail.com>
2018-09-19 17:54:01 -0400

    Tutorial 5 - changed network manager to lobby manager

---
+++ lobby1/LobbyTutorial1/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyManager : NetworkLobbyManager {
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        print("A game has created");
+    }
+}
---
+++ lobby1/LobbyTutorial1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public InputField MatchNameInput;
+    public LobbyManager lobbymanager;
+
+
+    public void OnClickHostButton() {
+        lobbymanager.StartMatchMaker();
+        lobbymanager.matchMaker.CreateMatch(MatchNameInput.text, (uint)lobbymanager.maxPlayers, true,
+                                            "", "", "", 0, 0, lobbymanager.OnMatchCreate);
+
+    }
+}
