cc9d9a9b4215ac11bbba4f32c21de7d1ee3cbeb0
Ro Anders <ro.c.anders@gmail.com>
2018-09-19 14:53:02 -0400

    Finished tutorial 3 & 4. Setup for matchmaking. Fixed bug

---
+++ lobby1/LobbyTutorial1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public float speed = 5;
+
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer) {
+            return;
+        }
+        var inputs = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.up * Time.deltaTime * inputs * speed);
+	}
+}
