bd88c784fb14af80d47c03948eda68d722644c16
Ro Anders <ro.c.anders@gmail.com>
2018-10-02 16:43:30 -0400

    Lobby tutorial 7 - though tutorial left off with it not quite working

--- lobby1/LobbyTutorial1/Assets/Scripts/Lobby_Player.cs
+++ lobby1/LobbyTutorial1/Assets/Scripts/Lobby_Player.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Lobby_Player : NetworkLobbyPlayer {
 
     public GameObject ParentPref;
+    public Button Joinbutton;
+    public Text MyText;
+    public Text ButtonText;
 
     public override void OnClientEnterLobby()
     {
         base.OnClientEnterLobby();
         ParentPref = GameObject.FindGameObjectWithTag("ParentPref");
         gameObject.transform.SetParent(ParentPref.transform);
+        Setup();
     }
 
+    private void Setup() {
+        if (isLocalPlayer) {
+            MyText.text = "MYPlayer";
+            Joinbutton.enabled = true;
+            ButtonText.text = "JOIN";
+        } else {
+            MyText.text = "Not me";
+            Joinbutton.enabled = false;
+            ButtonText.text = "...";
+        }
+    }
 }
