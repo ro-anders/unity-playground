1278686602216d2400ee92b2334686b8e4cce680
Ro Anders <ro.c.anders@gmail.com>
2018-10-22 22:58:02 -0400

    Finished tutorial 11

--- lobby1/LobbyTutorial1/Assets/Scripts/HostSetup.cs
+++ lobby1/LobbyTutorial1/Assets/Scripts/HostSetup.cs
@@ -10,10 +10,34 @@ public class HostSetup : MonoBehaviour
     public Text HostName;
 
     MatchInfoSnapshot match;
+    LobbyManager lobbymanager;
+    GameObject LobbyParent;
+
+    public void Start()
+    {
+        lobbymanager = GameObject.FindGameObjectWithTag("LMANAGER").GetComponent<LobbyManager>();
+        LobbyParent = GameObject.FindGameObjectWithTag("LOBBYPARENT");
+    }
 
     public void Setup(MatchInfoSnapshot inMatch)
     {
         match = inMatch;
         HostName.text = match.name;
     }
+
+    public void Join()
+    {
+        if (lobbymanager == null) {
+            lobbymanager = GameObject.FindGameObjectWithTag("LMANAGER").GetComponent<LobbyManager>();
+        }
+
+        //LobbyParent.SetActive(true);
+        var go = LobbyParent.GetComponentsInChildren<Transform>(true);
+        foreach(var item in go) {
+            item.gameObject.SetActive(true);
+        }
+
+        lobbymanager.matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, lobbymanager.OnMatchJoined);
+    }
+
 }
