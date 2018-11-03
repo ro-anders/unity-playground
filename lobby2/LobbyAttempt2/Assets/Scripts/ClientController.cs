68824e0c3eae6eb67e2ff02727fc21c6493d61ef
Ro Anders <ro.c.anders@gmail.com>
2018-11-03 08:08:08 -0400

    Created lobby scene that shows other players

---
+++ lobby2/LobbyAttempt2/Assets/Scripts/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ClientController : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text thisText = this.GetComponent<Text>();
+        NetworkIdentity id = this.GetComponent<NetworkIdentity>();
+        thisText.text = "Client: " + id.netId;
+
+        GameObject clientList = GameObject.FindGameObjectWithTag("ClientParent");
+        gameObject.transform.SetParent(clientList.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
