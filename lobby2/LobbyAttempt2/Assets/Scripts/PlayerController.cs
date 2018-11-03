f2943ac6ca1f70d9ef6f5934a2b58c95fba77a64
Ro Anders <ro.c.anders@gmail.com>
2018-11-03 00:43:55 -0400

    made a simple multiplayer scene

---
+++ lobby2/LobbyAttempt2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        transform.Translate(x, y, 0);
+    }
+}
