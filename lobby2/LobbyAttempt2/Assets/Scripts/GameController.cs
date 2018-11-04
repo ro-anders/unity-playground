ab98e6eb467568942614e24a33d4ab6f7c81b64b
Ro Anders <ro.c.anders@gmail.com>
2018-11-04 01:09:12 -0400

    Got proof of concept working that shows you can a lobby scene with a network manager and then a game scene with a completely different network manager.

---
+++ lobby2/LobbyAttempt2/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+
+    private const string GAME_MATCH_NAME = "h2h-1";
+
+    private NetworkManager lobbyManager;
+
+    public void ConnectToDefault()
+    {
+        Debug.Log("Connecting to game");
+        lobbyManager = this.GetComponent<NetworkManager>();
+        if (lobbyManager.matchMaker == null)
+        {
+            lobbyManager.StartMatchMaker();
+        }
+        lobbyManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, onMatchList);
+    }
+
+    private void onMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        if (!success)
+        {
+            Debug.Log("Error looking for default Lobby match.");
+            // we are going to refresh it again
+        }
+        else
+        {
+            MatchInfoSnapshot found = null;
+            if (matchList.Count > 0)
+            {
+                // There should be only one match ever - the default one
+                // but we check just in case
+                foreach (MatchInfoSnapshot match in matchList)
+                {
+                    if (match.name == GAME_MATCH_NAME)
+                    {
+                        found = match;
+                        break;
+                    }
+                }
+            }
+            if (found == null)
+            {
+                // No one has hosted yet.  Try to host.
+                lobbyManager.matchMaker.CreateMatch(GAME_MATCH_NAME, (uint)100, true,
+                                    "", "", "", 0, 0, lobbyManager.OnMatchCreate);
+            }
+            else
+            {
+                lobbyManager.matchMaker.JoinMatch(found.networkId, "", "", "", 0, 0, lobbyManager.OnMatchJoined);
+            }
+        }
+    }
+
+}
---
+++ lobby2/LobbyAttempt2/Assets/Scripts/LobbyController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour {
+
+    private const string LOBBY_MATCH_NAME = "h2h-default";
+
+    private NetworkManager lobbyManager;
+
+    private bool isHost = false;
+
+    public void OnStartPressed() {
+        Debug.Log("Start pressed");
+        lobbyManager = this.GetComponent<NetworkManager>();
+        if (lobbyManager.matchMaker == null)
+        {
+            lobbyManager.StartMatchMaker();
+        }
+        lobbyManager.matchMaker.ListMatches(0, 20, "", true, 0, 0, onMatchList);
+    }
+
+    private void onMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        if (!success)
+        {
+            Debug.Log("Error looking for default Lobby match.");
+            // we are going to refresh it again
+        }
+        else
+        {
+            MatchInfoSnapshot found = null;
+            if (matchList.Count > 0) {
+                // There should be only one match ever - the default one
+                // but we check just in case
+                foreach (MatchInfoSnapshot match in matchList)
+                {
+                    if (match.name == LOBBY_MATCH_NAME)
+                    {
+                        found = match;
+                        break;
+                    }
+                }
+            }
+            if (found == null)
+            {
+                // No one has hosted yet.  Try to host.
+                lobbyManager.matchMaker.CreateMatch(LOBBY_MATCH_NAME, (uint)100, true,
+                                    "", "", "", 0, 0, lobbyManager.OnMatchCreate);
+                isHost = true;
+            }
+            else
+            {
+                lobbyManager.matchMaker.JoinMatch(found.networkId, "", "", "", 0, 0, lobbyManager.OnMatchJoined);
+            }
+        }
+    }
+
+    public void OnPlayPressed() {
+        if (isHost) {
+            NetworkManager.singleton.StopHost();
+        } else {
+            NetworkManager.singleton.StopClient();
+        }
+        SceneManager.LoadScene("Game");
+    }
+}
